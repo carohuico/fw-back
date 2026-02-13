f60b79cffc09c67fe16dab187318fb0694daa54c
Carolina Huicochea <carolina.huicochea@softtek.com>
2026-02-13 14:18:17 -0600

    changed app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from scripts.config import Service
 app = FastAPI()
 
 
+import os
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", host=Service.host, port=Service.port)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
